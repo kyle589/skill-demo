97e1b14101cf61157e841e443953de50fc8804de
kyle589 <kyleng589@gmail.com>
2022-02-03 15:50:54 -0800

    lib + tester

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SkillDemoTest {
+    @Test
+    public void subtract(){
+        assertEquals(SkillDemo.subtract(5,2), 1)
+    }
+}
